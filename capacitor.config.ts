f31907eecadc984d80edacf4c9a4f898551b2c15
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2026-04-16 09:31:27 -0500

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.picsafe.app',
+  appId: 'com.picsafe.ps',
   appName: 'picsafe',
   webDir: 'build'
 };
